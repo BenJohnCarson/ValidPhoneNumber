4c83d20a1d824baabbd9c2b72e4aaa4112c4887c
Ben J. Carson <benjohncarson@gmail.com>
2016-11-23 17:22:30 +0000

    Refactored regex

--- ValidPhoneNumber.rb
+++ ValidPhoneNumber.rb
@@ -1,9 +1,5 @@
 def validPhoneNumber(phoneNumber)
-	test = phoneNumber.scan(/(\W{1}\d{3}\W{1}\s)(\d{3}\S{1})(\d{4})/)
+	test = phoneNumber.scan(/\W{1}\d{3}\W{1}\s\d{3}\S{1}\d{4}/)
 
 	test.join == phoneNumber ? true : false
 end
-
-print validPhoneNumber("(098) 123 4567")
-
-#Now passes all tests.
